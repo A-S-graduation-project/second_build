9e7b2b598c09f34d4314db2d882eba04491eacfa
Kimdaehwan <etukdh6433@naver.com>
2022-12-06 19:32:44 +0900

    create allergy based collabor

--- allergyProject/test-algorithm.py
+++ allergyProject/test-algorithm.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 # user data, product data 읽기
-choice_data = pd.read_csv('C:/Users/user/Project/second_build/allergyProject/UserData.csv')
+choice_data = pd.read_csv('C:/Users/user/Project/second_build/allergyProject/UserData.csv', encoding='cp949')
 product_data = pd.read_csv('C:/Users/user/Project/second_build/allergyProject/Product.csv')
 
 # product data 전처리
@@ -10,15 +11,19 @@ product_data.drop('allergy', axis=1, inplace=True)
 product_data.drop('prdkind', axis=1, inplace=True)
 product_data.drop('manufacture', axis=1, inplace=True)
 
-print(choice_data.head(2))
-print(product_data.head(2))
-
 # 병합
-product_choice_data = pd.merge(product_data, choice_data, on='prdlstNm')
-print(product_choice_data.head(5))
-
-# rating의 필요성?
+merge_data = pd.merge(product_data, choice_data, on='prdlstNm')
 
 # 데이터 분포 (오류)
-# data = product_choice_data.pivot_table('prdlstRepotNo', index='allergy', columns='prdlstNm')
-# print(data.head(5))
+allergy_product_data = merge_data.pivot_table('rating', index='allergy', columns='prdlstNm')
+# product_allergy_data = merge_data.pivot_table('rating', index='prdlstNm', columns='allergy')
+
+# NaN값 0으로 대체
+allergy_product_data.fillna(0, inplace=True)
+
+# 코사인 유사도 사용
+allergy_based_collabor = cosine_similarity(allergy_product_data)
+
+# allergy based 유사도 생성
+allergy_based_collabor = pd.DataFrame(data = allergy_based_collabor, index = allergy_product_data.index, columns = allergy_product_data.index)
+print(allergy_based_collabor.head())
