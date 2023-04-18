303fc980b7c26e2e3e924228f3af807b3b6e5693
Kimdaehwan <etukdh6433@naver.com>
2023-04-18 20:45:56 +0900

    fix login.html and signup.html

--- allergyProject/signapp/urls.py
+++ allergyProject/signapp/urls.py
@@ -5,6 +5,7 @@ app_name = 'signapp'
 
 urlpatterns = [
     path('login/', signviews.Login, name='login'),
+    path('signup/', signviews.Signup , name='signup'),
     path('mypage/', signviews.Mypage, name='mypage'),
-    # path('/', signviews.Logout , name='logout'),
+
 ]
--- allergyProject/signapp/views.py
+++ allergyProject/signapp/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def Login (request):
-    return render(request, 'testlogin.html')
+    return render(request, 'login.html')
+
+def Signup (request):
+    return render(request, 'signup.html')
 
 def Mypage (request):
     return render(request, 'mypage.html')
